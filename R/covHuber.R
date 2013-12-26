0dfa82ff15721612c1742abdb6511cb31c056539
Andreas Alfons <alfons@ese.eur.nl>
2013-12-26 23:55:26 +0100

    rewrote covHuber()

--- R/covHuber.R
+++ R/covHuber.R
@@ -54,37 +54,47 @@ covHuber <- function(x, control = covHuber.control(...), ...) {
   if(!is.finite(tol) || tol < 0) tol <- 0
   maxIterations <- control$maxIterations
   if(!is.finite(maxIterations) || maxIterations < 0) maxIterations <- 0
-  # define tuning parameters 
-  # tau is chosen such that E[chi_p^2 u_2(chi_p^2)] = p (i.e., such that Sigma 
-  # is asymptotically unbiased)
-  kappa <- 1 - prob
-  r <- sqrt(qchisq(prob, df=p))  # cutoff point
-  tau <- (-(r^p * exp(-r^2/2))/(2^(p/2-1)*gamma(p/2))+p*(prob)+r^2*kappa)/p
-  # compute starting values for location vector and scatter matrix
+  ## compute starting values for location vector and scatter matrix
   mu <- colMeans(x)
   Sigma <- cov(x)
   ## perform iterative reweighting
   i <- 0
-  continue <- TRUE
-  while(continue && i < maxIterations) {
-    oldMu <- mu
-    oldSigma <- Sigma
-    # compute weights based on mahalanobis distances
-    d <- sqrt(mahalanobis(x, center=mu, cov=Sigma))  # mahalanobis distances
-    u <- ifelse(d > r, r/d, 1)
-    # update location vector and scatter matrix
-    mu <- apply(x, 2, weighted.mean, w=u)
-    Sigma <- crossprod(u * sweep(x, 2, mu, check.margin=FALSE)) / (n*tau)
-    # check convergence
-    i <- i + 1
-    continue <- max(abs(mu-oldMu)) > tol || max(abs(Sigma-oldSigma)) > tol
+  if(prob < 1) {
+    # define tuning parameters
+    # tau is chosen such that E[chi_p^2 u^2(chi_p^2)] / tau = p
+    # (i.e., such that Sigma is asymptotically unbiased)
+    kappa <- 1 - prob
+    r <- qchisq(prob, df=p)  # squared cutoff point
+    tau <- (-2*dchisq(r, df=p) + kappa) * r / p + prob
+    # perform iterative reweighting
+    continue <- TRUE
+    while(continue && i < maxIterations) {
+      oldMu <- mu
+      oldSigma <- Sigma
+      # compute weights based on mahalanobis distances
+      d <- mahalanobis(x, center=mu, cov=Sigma)  # squared mahalanobis distances
+      u <- sqrt(ifelse(d > r, r/d, 1))
+      # update location vector and scatter matrix
+      mu <- apply(x, 2, weighted.mean, w=u)
+      Sigma <- crossprod(u * sweep(x, 2, mu, check.margin=FALSE)) / (n*tau)
+      # check convergence
+      i <- i + 1
+      continue <- max(abs(mu-oldMu)) > tol || max(abs(Sigma-oldSigma)) > tol
+    }
+    # check if algorithm converged
+    if(i == maxIterations && continue) {
+      warning(sprintf("no convergence in %d iterations", maxIterations))
+    }
+    # compute weights
+    if(i > 0) weights <-  u / sqrt(tau)
   }
   ## return results
-  if(i == 0) u <- rep.int(1, n)
-  if(i == maxIterations && continue) {
-    warning(sprintf("no convergence in %d iterations", maxIterations))
+  if(i == 0) {
+    tau <- 1
+    weights <- rep.int(1, n)
   }
-  result <- list(center=mu, cov=Sigma, weights=u/sqrt(tau), nIterations=i)
+  result <- list(center=mu, cov=Sigma, weights=weights, nIterations=i, 
+                 prob=prob, tau=tau)
   class(result) <- "covHuber"
   result
 }
