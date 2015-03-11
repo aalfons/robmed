2904b81511d3f38afb7fe4c894d215b92014a311
Andreas Alfons <alfons@ese.eur.nl>
2015-03-11 11:43:22 +0100

    cleaned up code

--- R/summary.R
+++ R/summary.R
@@ -5,57 +5,6 @@
 
 ## summary of a mediation model fit based on a scatter matrix
 #' @S3method summary covFitMediation
-# summary.covFitMediation <- function(object, ...) {
-#   # extract covariance matrix
-#   S <- object$cov$cov
-#   # extract variable names
-#   cn <- names(object$data)
-#   x <- cn[1]
-#   y <- cn[2]
-#   m <- cn[3]
-#   # extract number of observations
-#   n <- nobs(object$cov)
-#   # compute the inverse of the Fisher information matrix for the unique 
-#   # elements of the covariance matrix
-#   D <- duplicationMatrix(3)
-#   invS <- solve(S)
-#   W <- t(D) %*% kronecker(invS, invS) %*% D / 2
-#   OmegaSigma <- solve(W)
-#   # apply the delta method to obtain the inverse of the Fisher information 
-#   # matrix for the coefficients in the mediation model
-#   d <- S[x,x]*S[m,m] - S[m,x]^2
-#   dsq <- d^2
-#   hDot <- matrix(c(-S[m,x]/S[x,x]^2, 0, 1/S[x,x], 0, 0, 0,   # a
-#                    (S[m,x]^2*S[y,m]+S[m,x]*S[y,x]*S[m,m])/dsq, -S[m,x]/d, 
-#                    (-S[y,x]*S[x,x]*S[m,m]-S[m,x]^2*S[y,x]+2*S[x,x]*S[y,m]*S[m,x])/dsq,
-#                    (S[m,x]*S[y,x]*S[x,x]-S[x,x]^2*S[y,m])/dsq, S[x,x]/d, 0,  # b
-#                    (-S[m,m]^2*S[y,x]+S[m,x]*S[y,m]*S[m,m])/dsq, S[m,m]/d, 
-#                    (-S[y,m]*S[x,x]*S[m,m]-S[m,x]^2*S[y,m]+2*S[m,m]*S[y,x]*S[m,x])/dsq, 
-#                    (-S[m,x]^2*S[y,x]+S[m,x]*S[y,m]*S[x,x])/dsq, -S[m,x]/d, 0,  # c
-#                    -S[y,x]/S[x,x]^2, 1/S[x,x], 0, 0, 0, 0),  # c'
-#                  6, 4)
-#   OmegaTheta <- t(hDot) %*% OmegaSigma %*% hDot
-#   # compute standard errors
-#   se <- sqrt(diag(OmegaTheta) / n)
-#   # perform asymptotic tests
-#   coefficients <- c(object$a, object$b, object$c, object$cPrime)
-#   z <- coefficients / se
-#   pValue <- pValueZ(z)
-#   coefficients <- cbind(coefficients, se, z, pValue)
-#   tn <- c("Estimate", "Std. Error", "z value", "Pr(>|z|)")
-#   dimnames(coefficients) <- list(c(x, m, x, x), tn)
-#   # compute residual standard error
-#   s <- S[y,y]-object$b^2*S[m,m]-object$c^2*S[x,x]-2*object$b*object$c*S[m,x]
-#   s <- list(value=s)
-#   # return results
-#   result <- list(a=coefficients[1, , drop=FALSE], 
-#                  b=coefficients[2, , drop=FALSE], 
-#                  c=coefficients[3, , drop=FALSE], 
-#                  cPrime=coefficients[4, , drop=FALSE], 
-#                  robust=object$robust, s=s, n=n, variables=cn)
-#   class(result) <- "summaryFitMediation"
-#   result
-# }
 summary.covFitMediation <- function(object, ...) {
   # extract variable names
   cn <- names(object$data)
