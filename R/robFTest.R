da5a6e08625782f46e8a49b6bc287bcdb9cb1113
Andreas Alfons <alfons@ese.eur.nl>
2017-07-20 14:43:35 +0200

    robust F-test no longer needs Fortran code and uses functions from 'robustbase' instead (closes #7)

--- R/robFTest.R
+++ R/robFTest.R
@@ -4,7 +4,6 @@
 # --------------------------------------
 
 ## compute a robust F-test (which actually uses the chi-squared distribution)
-#' @useDynLib robmed
 
 # generic function
 robFTest <- function(object, ...) UseMethod("robFTest")
@@ -13,7 +12,7 @@ robFTest <- function(object, ...) UseMethod("robFTest")
 robFTest.regFitMediation <- function(object, ...) {
   # check if Tukey bisquare loss function is used, otherwise return NULL
   control <- object$control
-  if (control$psi != "bisquare") return()
+  if (control$psi != "bisquare" || is.null(control$efficiency)) return()
   # extract the full model and fit the null model
   fitFull <- object$fitYMX
   fY <- as.formula(paste(object$y, "~ 1"))
@@ -21,56 +20,16 @@ robFTest.regFitMediation <- function(object, ...) {
   # perform robust F-test
   s <- fitFull$scale
   df <- fitFull$rank - fitNull$rank
-  FTau <- (2 / df) * sum(chi.weight(residuals(fitNull)/s, control$tuning.psi) -
-                           chi.weight(residuals(fitFull)/s, control$tuning.psi))
-  tmp <- lmRob.eff0(control$tuning.psi)
-  const <- tmp$alfa / tmp$beta
-  # if X ~ F(df1, df2), then Y = df1*X ~ chi-squared(df1)
-  statistic <- FTau / (const * df)
-  pValue <- pchisq(FTau / const, df = df, lower.tail = FALSE)
+  chiFull <- Mchi(residuals(fitFull) / s, control$tuning.psi, control$psi)
+  chiNull <- Mchi(residuals(fitNull) / s, control$tuning.psi, control$psi)
+  tau <- (2 / df) * sum(chiNull - chiFull)
+  if(control$efficiency == 0.8)       const <- 0.4140647
+  else if(control$efficiency == 0.85) const <- 0.3572601  # default
+  else if(control$efficiency == 0.9)  const <- 0.2953806
+  else if(control$efficiency == 0.95) const <- 0.2180425
+  # Y = df1*X ~ chi-squared(df1) if X ~ F(df1, df2) and df2 goes to infinity
+  chi2 <- tau / const
+  pValue <- pchisq(chi2, df = df, lower.tail = FALSE)
   # return results
-  list(statistic = statistic, df = c(df, Inf), pValue = pValue)
-}
-
-
-## utility functions
-
-# These are modified from package 'robust'.  Unfortunately, package 'robust'
-# does not export those functions.  The original Fortran code is not modified.
-
-# weight function
-chi.weight <- function(x, tuning.psi) {
-  n <- length(x)
-  result <- .Fortran("rlchiam2",  # ../src/lmrobmm.f
-           as.integer(n),
-           as.double(x),
-           fvals = double(n),
-           2L,  # this (integer) value indicates to use Tukey bisquare loss
-           as.double(tuning.psi),
-           PACKAGE = "robmed")
-  result$fvals
-}
-
-# asymptotic relative efficiency (ARE) of a GM-estimator (Tukey bisquare loss):
-lmRob.eff0 <- function(tuning.psi, upper = 10, til = 1e-4, maxit = 150,
-                       tol = 1e-5, eps = .Machine$double.eps,
-                       uflow = .Machine$double.xmin) {
-  index <- c(1, 1, 1,
-             2, # this value indicates to use Tukey bisquare loss
-             1, 1, 0)
-  tuning <- c(0, 0, tuning.psi, 0, tuning.psi, eps, uflow, upper, til)
-  result <- .Fortran("rlref0bi",  # ../src/lmrobbi.f
-           as.integer(index),
-           as.double(tuning),
-           xlcnst = -1.0,
-           1L,
-           1.0,
-           as.integer(maxit),
-           as.double(tol),
-           nit = 0L,
-           alfa = 0.0,
-           beta = 0.0,
-           reff = 0.0,
-           PACKAGE = "robmed")
-  result[c("nit", "alfa", "beta", "reff")]
+  list(statistic = chi2 / df, df = c(df, Inf), pValue = pValue)
 }
