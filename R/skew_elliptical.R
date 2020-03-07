3e6eca3b9e897ee08211562f27855615f8fbf09b
Andreas Alfons <alfons@ese.eur.nl>
2020-03-07 18:06:38 +0100

    added function to fit regression with skew-elliptical errors

---
+++ R/skew_elliptical.R
@@ -0,0 +1,39 @@
+# --------------------------------------
+# Author: Andreas Alfons
+#         Erasmus Universiteit Rotterdam
+# --------------------------------------
+
+
+# regression with skew-elliptical errors
+#' @importFrom sn selm.fit
+#' @importFrom methods new
+selm_fit <- function(x, y, intercept = TRUE, family = "ST",
+                     fixed.param = list()) {
+  # # selm.fit() requires matrix of predictors, not data frame
+  # x <- as.matrix(x)
+  # selm.fit() always requires constant for intercept, so argument is ignored
+  n <- nrow(x)
+  x <- cbind("(Intercept)" = rep.int(1, n), x)
+  # fit the linear model
+  control <- list(method = "MLE")
+  fit <- selm.fit(x, y, family = family, fixed.param = fixed.param,
+                  selm.control = control)
+  # return model fit as object of S4 class "selm"
+  # setClass(fit) <- "selm"
+  new("selm", call = call("selm"), family = family, logL = fit$logL,
+      method = control$method, param = fit$param, param.var = fit$param.var,
+      size = fit$size, residuals.dp = fit$resid.dp,
+      fitted.values.dp = fit$fitted.dp, control = list(), input = list(),
+      opt.method = fit$opt.method)
+}
+
+
+## define a coef() method as the one from package 'sn' in some cases doesn't
+## actually return the regression coefficients (and also returns additional
+## parameters)
+#' @importFrom methods setMethod
+setMethod("coef", "selm", function(object, ...) {
+  coefficients <- object@param$dp
+  keep <- setdiff(names(coefficients), c("omega", "alpha", "nu"))
+  coefficients[keep]
+})
